b041cb71674d65c5dc530f2c708f32807afa6a61
majuran <majurageerthan@villvay.com>
2023-02-01 11:36:45 +0530

    Added producer and consumer in ActiveMQ

---
+++ src/main/java/com/villvay/rd/activemqperformance/controller/ProduceMessageController.java
@@ -0,0 +1,23 @@
+package com.villvay.rd.activemqperformance.controller;
+
+import com.villvay.rd.activemqperformance.model.Employee;
+import com.villvay.rd.activemqperformance.producer.JmsProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ProduceMessageController {
+
+  @Autowired
+  JmsProducer jmsProducer;
+
+  @PostMapping(value="/api/employee")
+  public Employee sendMessage(@RequestBody Employee employee){
+    jmsProducer.sendMessage(employee);
+    return employee;
+  }
+}
---
+++ src/main/java/com/villvay/rd/activemqperformance/model/Employee.java
@@ -0,0 +1,9 @@
+package com.villvay.rd.activemqperformance.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+  private int id;
+  private String name;
+}
