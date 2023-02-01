5a62544ea3c33cd090a81cb196569e4ff2445965
majuran <majurageerthan@villvay.com>
2023-02-01 11:35:52 +0530

    Initial commit

---
+++ src/main/java/com/villvay/rd/activemqperformance/ActiveMqPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.villvay.rd.activemqperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveMqPerformanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/villvay/rd/activemqperformance/ActiveMqPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.villvay.rd.activemqperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
