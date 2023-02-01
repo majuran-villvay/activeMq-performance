d050452d6d147f1719c48c8dddb3bda61f474f39
majuran <majurageerthan@villvay.com>
2023-02-01 12:09:14 +0530

    Added consumer

--- src/main/java/com/villvay/rd/activemqperformance/config/ActiveMQConfig.java
+++ src/main/java/com/villvay/rd/activemqperformance/config/ActiveMQConfig.java
@@ -1,35 +1,57 @@
 package com.villvay.rd.activemqperformance.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
 import javax.jms.ConnectionFactory;
-import java.util.Arrays;
 
+//https://spring.io/guides/gs/messaging-jms/
 @Configuration
 @EnableJms
+@Slf4j
 public class ActiveMQConfig {
 
   @Value("${active-mq.broker-url}")
   private String brokerUrl;
 
+//  @Bean
+//  public ConnectionFactory connectionFactory() {
+//    ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
+//    activeMQConnectionFactory.setBrokerURL(brokerUrl);
+//    activeMQConnectionFactory.setTrustedPackages(List.of("com.mailshine.springbootstandaloneactivemq"));
+//    return activeMQConnectionFactory;
+//  }
+
   @Bean
-  public ConnectionFactory connectionFactory(){
-    ActiveMQConnectionFactory activeMQConnectionFactory  = new ActiveMQConnectionFactory();
-    activeMQConnectionFactory.setBrokerURL(brokerUrl);
-    activeMQConnectionFactory.setTrustedPackages(Arrays.asList("com.mailshine.springbootstandaloneactivemq"));
-    return  activeMQConnectionFactory;
+  public ConnectionFactory connectionFactory() {
+    log.info("Connecting to Active MQ broker at {}", brokerUrl);
+    return new ActiveMQConnectionFactory(brokerUrl);
   }
 
   @Bean
-  public JmsTemplate jmsTemplate(){
+  public JmsListenerContainerFactory<?> topicListenerFactory(ConnectionFactory connectionFactory,
+                                                             DefaultJmsListenerContainerFactoryConfigurer configurer) {
+    DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+    // the configurer will use PubSubDomain from application.properties if defined or false if not
+    //so setting it on the factory level need to be set after this
+    configurer.configure(factory, connectionFactory);
+    factory.setPubSubDomain(true);
+    return factory;
+  }
+
+  @Bean
+  public JmsTemplate jmsTemplate() {
     JmsTemplate jmsTemplate = new JmsTemplate();
     jmsTemplate.setConnectionFactory(connectionFactory());
     jmsTemplate.setMessageConverter(jacksonJmsMessageConverter());
--- src/main/java/com/villvay/rd/activemqperformance/consumer/JmsConsumer.java
+++ src/main/java/com/villvay/rd/activemqperformance/consumer/JmsConsumer.java
@@ -5,24 +5,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-import javax.jms.Message;
-import javax.jms.MessageListener;
-import javax.jms.ObjectMessage;
-
 @Component
 @Slf4j
-public class JmsConsumer implements MessageListener {
-  @Override
-  @JmsListener(destination = "${active-mq.topic}")
-  public void onMessage(Message message) {
-    try{
-      ObjectMessage objectMessage = (ObjectMessage)message;
-      Employee employee = (Employee)objectMessage.getObject();
-      //do additional processing
-      log.info("Received Message: "+ employee.toString());
-    } catch(Exception e) {
-      log.error("Received Exception : "+ e);
-    }
+public class JmsConsumer {
+//  @JmsListener(destination = "${active-mq.topic}")
+//  public void onMessage(Message message) {
+//    try{
+//      ObjectMessage objectMessage = (ObjectMessage)message;
+//      Employee employee = (Employee)objectMessage.getObject();
+//      //do additional processing
+//      log.info("Received Message: "+ employee.toString());
+//    } catch(Exception e) {
+//      log.error("Received Exception : "+ e);
+//    }
+//
+//  }
 
+  @JmsListener(destination = "${active-mq.topic}", containerFactory = "topicListenerFactory")
+  public void receiveMessage(Employee email) {
+    log.info("Received <" + email + ">");
   }
 }
